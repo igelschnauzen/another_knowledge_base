ebbe4e9afacd3da299243a763ec46e41be1c3988
igelschnauzen <kriegg878@gmail.com>
2024-12-10 02:20:30 +0200

    specs

--- src/users/jwt.strategy.ts
+++ src/users/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'supersecretkey', // Replace with your secret
+      secretOrKey: 'supersecretkey',
     });
   }
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,7 +3,7 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 
-@Controller('auth')
+@Controller('users')
 export class UsersController {
   constructor(private readonly authService: UsersService) {}
 
